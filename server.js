ee3c034015ef7af8772541fe725ac53f87da36f2
Md Faizan <mdfaizan@Mds-MacBook-Air.local>
2025-07-02 15:49:03 +0530

    DB connection

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const port = process.env.PORT || 8000;
 
 dotenv.config()
 
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+ .then(()=>{
+    console.log('MongoDB Connected sucessfully!!')
+ })
+ .catch((err) =>{
+    console.log("Failed to connect MongoDB",err.message)
+ })
+
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`)
 })
